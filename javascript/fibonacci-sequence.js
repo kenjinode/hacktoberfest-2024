2c0c71b39baecca6b4ae70247b2f8a6f2356dd0b
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2024-10-29 19:38:29 +0530

    Add array flattening & fibonacci sequence function

---
+++ javascript/fibonacci-sequence.js
@@ -0,0 +1,9 @@
+function fibonacci(n) {
+    const fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    return fib.slice(0, n);
+}
+
+console.log(fibonacci(10)); 
