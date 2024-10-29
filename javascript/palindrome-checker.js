a743729fae46f5a8faaf404fe0fe343694c0de80
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2024-10-29 19:47:42 +0530

    Update palindrome-checker.js

--- javascript/palindrome-checker.js
+++ javascript/palindrome-checker.js
@@ -2,6 +2,5 @@ function isPalindrome(str) {
   const cleanedStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
   return cleanedStr === cleanedStr.split("").reverse().join("");
 }
-
 console.log(isPalindrome("A man, a plan, a canal, Panama"));
 console.log(isPalindrome("Hello"));
